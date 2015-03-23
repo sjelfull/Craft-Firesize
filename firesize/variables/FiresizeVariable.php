d6243fde9ee440f275ba3db16c13e604a526c13c
Fred Carlsen <fred@sjelfull.no>
2015-03-23 14:06:35 +0100

    Add license to files

--- firesize/variables/FiresizeVariable.php
+++ firesize/variables/FiresizeVariable.php
@@ -2,6 +2,17 @@
 
 namespace Craft;
 
+/**
+ * Firesize for Craft, by Fred Carlsen
+ *
+ * @author      Fred Carlsen <http://sjelfull.no>
+ * @package     Firesize
+ * @since       Craft 1.3
+ * @copyright   Copyright (c) 2015, Fred Carlsen
+ * @license     http://opensource.org/licenses/mit-license.php MIT License
+ * @link        https://github.com/sjelfull/Craft-Firesize
+ */
+
 class FiresizeVariable {
 
     public function url($urlOrOptions = null)
